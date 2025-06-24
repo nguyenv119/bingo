b17661e960abaf3c7c41ad36887a51db01615d0e
nguyenv119ue <nguyenv@brandeis.edu>
2025-06-24 00:33:56 -0400

    Updates

---
+++ _next/static/Z9YwpG38O-3zcHSNGQj-M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/611-7179f3795d19c968.js","static/chunks/pages/index-fd83ebb4d8688b12.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
